ab18c38787ba4b1cd27643d235d2e7c27147a5eb
Fathima Salam <fathimasalamta@gmail.com>
2025-10-31 23:44:03 +0530

    bubble Sort

---
+++ DSA/sortingAlgorithms/bubbleSort.js
@@ -0,0 +1,15 @@
+let marks = [45, 78, 23, 89, 12, 67];
+
+function bubbleSort(arr){
+    let n = arr.length;
+    for(let i=0 ; i<n ; i++){
+        for(let j=0;j<n-i-1;j++){
+            if(arr[j]<arr[j+1]){
+                [arr[j],arr[j+1]]=[arr[j+1],arr[j]];
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(marks));
